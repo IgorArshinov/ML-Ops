74ef3f64014c7fd59061c7e0edeeeeb358141c34
azerty-github <azerty_1991@hotmail.com>
2025-05-27 17:15:46 +0200

    Adding the TerraForm files

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,26 +2,26 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.97.0"
+      version = ">=5.97"
     }
   }
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = var.aws_region
 }
 
-resource "random_string" "bucket_suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "some-example-${random_string.bucket_suffix.result}"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+# resource "random_string" "bucket_suffix" {
+#   length  = 8
+#   special = false
+#   upper   = false
+# }
+# 
+# resource "aws_s3_bucket" "example" {
+#   bucket = "some-example-${random_string.bucket_suffix.result}"
+# 
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
