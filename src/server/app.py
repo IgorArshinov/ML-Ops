e4dda6cb6dc666e535dd95708a03ab45f5ecbab1
azerty-github <azerty_1991@hotmail.com>
2025-06-10 18:13:17 +0200

    Updated the branch files

--- src/server/app.py
+++ src/server/app.py
@@ -1,9 +1,11 @@
+import yaml
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
 import joblib
 
 app = FastAPI()
+
 
 class InputData(BaseModel):
     Gender: str
@@ -14,16 +16,21 @@ class InputData(BaseModel):
     PastAccident: str
     AnnualPremium: float
 
-model = joblib.load('models/model.pkl')
+
+with open('./config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+model = joblib.load("models/" + config.get("model").get("full_file_name"))
+
 
 @app.get("/")
 async def root():
     return {"health_check": "200"}
 
+
 @app.post("/predict")
 async def predict(input_data: InputData):
-    
-        df = pd.DataFrame([input_data.model_dump().values()], 
-                          columns=input_data.model_dump().keys())
-        pred = model.predict(df)
-        return {"predicted_class": int(pred[0])}
+    df = pd.DataFrame([input_data.model_dump().values()],
+                      columns=input_data.model_dump().keys())
+    pred = model.predict(df)
+    return {"predicted_class": int(pred[0])}
